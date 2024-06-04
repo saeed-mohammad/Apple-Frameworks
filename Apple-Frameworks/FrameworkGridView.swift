26ac99d1a9b810c9e9fdb41458b23c85073c4fe1
Mohammad saeed <sk.saeed008@gmial.com>
2024-06-04 23:08:35 +0530

    grid, scroll and navigation

---
+++ Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,53 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by saeed shaikh on 6/3/24.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    let column: [GridItem] = [GridItem(.flexible()),
+                              GridItem(.flexible()),
+                              GridItem(.flexible())]
+    var body: some View {
+        NavigationView {
+            ScrollView{
+                LazyVGrid(columns: column){
+                    ForEach(MockData.frameworks) { framework in
+                        IconView(framework: framework)
+                    }
+                    
+                }
+            }
+            .navigationTitle("Frameworks")
+        }
+        
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+//            .preferredColorScheme(.dark)
+    }
+}
+
+struct IconView: View{
+    let framework: Framework
+    
+    var body: some View {
+        VStack{
+            Image(framework.imageName)
+                .resizable()
+                .frame(width: 80, height: 80)
+                .aspectRatio(contentMode: .fit)
+            Text(framework.name)
+                .font(.system(size: 16, weight: .medium, design: .default))
+                .scaledToFit()
+                .minimumScaleFactor(0.5)
+        }
+        .padding()
+    }
+}
